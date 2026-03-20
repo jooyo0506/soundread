62deb9f3fe18457fdd24baaacfec32e320e4e29a
zhouyou <zhouyouismy@gmail.com>
2026-03-21 00:25:11 +0800

    refactor: 清理无用代码
    - 删除边听边问功能全部代码（未完成/未注册/未上线）:
      InteractionWebSocketHandler / AiInteractionService / AiInteractionMapper / AiInteraction / WhisperAdapter
    - CreationController: 删除未使用的 storageQuotaService
    - AdminWorkService: 删除未使用的 userStorageMapper
    - ToneMatchService: 删除未使用的 cachedRoles
    - Tts1Client: 删除未使用的 TtsRequest 和 Collectors import
    - 删除旧 resources/docs/ 目录（RAG 已迁移至 resources/knowledge/）

--- server/src/main/java/com/soundread/mapper/AiInteractionMapper.java
+++
@@ -1,9 +0,0 @@
-package com.soundread.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.soundread.model.entity.AiInteraction;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface AiInteractionMapper extends BaseMapper<AiInteraction> {
-}
